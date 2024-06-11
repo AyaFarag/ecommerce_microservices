c69f95e15df7cc41bf22f3615f531717cf636a66
Aya Farag <ayasaid94.as@gmail.com>
2024-06-11 03:31:24 +0300

    CQRS

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,34 @@
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+
+namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<String> Category, string Description, string Image, double Price) 
+        : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid id);
+    public class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // create product from command object
+            // save to database
+            // return result
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.Image,
+                Price = command.Price,
+                // need category list
+
+            };
+
+            return new CreateProductResult(Guid.NewGuid());
+            
+            
+        }
+    }
+}
